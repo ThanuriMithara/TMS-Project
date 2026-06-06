aef5a4ba97dd22832305306cf6d408ff1641251a
Darani Wickramasinghe <wickram-im23119@stu.kln.ac.lk>
2026-06-06 22:55:10 +0530

    Add websocket system

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { initSocket } from './src/sockets/socketHandler.js';
+
 dotenv.config();
 
 const app = express();
 
-// Middleware
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
+
+
+initSocket(io);
+
+app.set('io', io);
+
+
 app.use(helmet());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
-// Routes
+
 import authRoutes from './src/routes/authRoutes.js';
 import userRoutes from './src/routes/userRoutes.js';
 import taskRoutes from './src/routes/taskRoutes.js';
@@ -22,12 +41,12 @@ app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/notifications', notificationRoutes);
 
-// Health check
+
 app.get('/', (req, res) => {
   res.json({ message: 'TMS Backend is running!' });
 });
 
-// Global error handler
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.status || 500).json({
@@ -37,6 +56,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
